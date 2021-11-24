32214ad92c85d10c1203ccfbd2a4a3afe06ad2e0
Log G CHENG <lcheng@hk.palo-it.com>
2021-11-24 15:59:05 +0800

    seo

--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -1,16 +1,23 @@
-import * as React from 'react';
-import { PlasmicCanvasHost } from '@plasmicapp/loader-nextjs';
-import Script from 'next/script';
-import { PLASMIC } from '../plasmic-init';
+import * as React from "react";
+import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import Script from "next/script";
+import { PLASMIC } from "../plasmic-init";
+import { NextSeo } from "next-seo";
 
 export default function Host() {
-  return PLASMIC && (
-    <div>
-      <Script
-        src="https://static1.plasmic.app/preamble.js"
-        strategy="beforeInteractive"
-      />
-      <PlasmicCanvasHost />
-    </div>
+  return (
+    PLASMIC && (
+      <div>
+        <NextSeo
+          title="Century Lab Innovation"
+          description="Century Lab Innovation is a global IT company providing innovative prototyping and market testing services for Companies. We provide creative, customized, and high-quality solutions to help clients accelerate their time-to-market."
+        />
+        <Script
+          src="https://static1.plasmic.app/preamble.js"
+          strategy="beforeInteractive"
+        />
+        <PlasmicCanvasHost />
+      </div>
+    )
   );
 }
