83f6643aff293ced82e0c3d2e673aca34fcaf822
Log G CHENG <lcheng@hk.palo-it.com>
2021-11-25 10:16:42 +0800

    adding title

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 import { PlasmicComponent } from "@plasmicapp/loader-nextjs";
 import { GetStaticPaths, GetStaticProps } from "next";
-
+import Script from "next/script";
 import {
   ComponentRenderData,
   PlasmicRootProvider,
 } from "@plasmicapp/loader-react";
 import Error from "next/error";
 import { PLASMIC } from "../plasmic-init";
+
+import Head from "next/head";
 
 export default function PlasmicLoaderPage(props: {
   plasmicData?: ComponentRenderData;
@@ -17,32 +19,54 @@ export default function PlasmicLoaderPage(props: {
     return <Error statusCode={404} />;
   }
   return (
-    <PlasmicRootProvider
-      loader={PLASMIC}
-      prefetchedData={plasmicData}
-    >
-      <PlasmicComponent component={plasmicData.entryCompMetas[0].name} />
-    </PlasmicRootProvider>
+    <>
+      <Head>
+        <title>Century Lab Innovation</title>
+        <meta property="og:title" content="CLI" key="title" />
+        <script>{"// {% seo %}"}</script>
+      </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PEKZYG5K8S"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-PEKZYG5K8S');
+        `}
+      </Script>
+      <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData}>
+        <PlasmicComponent component={plasmicData.entryCompMetas[0].name} />
+      </PlasmicRootProvider>
+    </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const { catchall } = context.params ?? {};
-  const plasmicPath = typeof catchall === 'string' ? catchall : Array.isArray(catchall) ? `/${catchall.join('/')}` : '/';
+  const plasmicPath =
+    typeof catchall === "string"
+      ? catchall
+      : Array.isArray(catchall)
+      ? `/${catchall.join("/")}`
+      : "/";
   const plasmicData = await PLASMIC.maybeFetchComponentData(plasmicPath);
   if (plasmicData) {
     return {
       props: { plasmicData },
 
       // Use revalidate if you want incremental static regeneration
-      revalidate: 60
+      revalidate: 60,
     };
   }
   return {
     // non-Plasmic catch-all
     props: {},
   };
-}
+};
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const pageModules = await PLASMIC.fetchPages();
@@ -57,4 +81,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
     // in Plasmic to be automatically available
     fallback: false,
   };
-}
+};
--- pages/plasmic-host.tsx
+++ pages/plasmic-host.tsx
@@ -8,19 +8,6 @@ export default function Host() {
   return (
     PLASMIC && (
       <div>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-PEKZYG5K8S"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-PEKZYG5K8S');
-        `}
-        </Script>
         <NextSeo
           title="Century Lab Innovation"
           description="Century Lab Innovation is a global IT company providing innovative prototyping and market testing services for Companies. We provide creative, customized, and high-quality solutions to help clients accelerate their time-to-market."
